ff84bda25bcf226305a2cc73fadadce887f7fcbd
Anushka <anushka@anushka-laptop.(none)>
2010-12-20 16:41:12 +0530

    Validation

--- db/migrate/20101219104037_add_name_value_to_products.rb
+++
@@ -1,9 +0,0 @@
-class AddNameValueToProducts < ActiveRecord::Migration
-  def self.up
-    add_column :products, :NameValue, :integer ,:default => 1
-  end
-
-  def self.down
-    remove_column :products, :NameValue
-  end
-end
