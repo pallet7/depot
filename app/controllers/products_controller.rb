15b8d125a5cc1bbd3282c8c115ea78e2e6d381a6
Anushka Modi <pallet7@gmail.com>
2010-12-26 15:53:37 +0530

    incomplete checkout order

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,16 @@ class ProductsController < ApplicationController
     end
   end
 
+# who_bought
+ def who_bought
+ @product = Product.find(params[:id])
+ respond_to do |format|
+ format.atom
+ format.xml { render :xml => @product }
+ end
+ end
+
+
   # DELETE /products/1
   # DELETE /products/1.xml
   def destroy
